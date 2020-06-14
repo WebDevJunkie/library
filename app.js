dffe256c3f54fced941ca217e8c97ae791b13b8c
Mike Wittenauer <mikewittenauer@me.com>
2020-06-14 17:44:11 -0400

    Setup templating engine to prepare for app development

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const port = process.env.PORT || 3000;
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.render('index', { list: ['a', 'b'] });
+    res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => { 
