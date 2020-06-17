9f06b213497f6be5e65a787eb370825b5556da01
Mike Wittenauer <mikewittenauer@me.com>
2020-06-17 15:17:30 -0400

    Books route

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get((req, res) => {
+        res.send('hello books');
+    });
 
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-    res.render('index', { list: ['a', 'b'], title: 'Library' });
+    res.render(
+        'index', 
+        { 
+            nav: [{ link: '/authors', title: 'Authors' }, { link: '/books', title: 'Books' }], 
+            title: 'Library'
+        }
+    );
 });
 
 app.listen(port, () => { 
